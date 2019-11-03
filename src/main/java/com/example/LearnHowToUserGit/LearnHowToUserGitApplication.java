86b51ce76cc24ce98e02c4dbcccf677b2ddf569f
zhaoheting <492995407@qq.com>
2019-11-03 16:08:33 +0800

    init

---
+++ src/main/java/com/example/LearnHowToUserGit/LearnHowToUserGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.LearnHowToUserGit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnHowToUserGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnHowToUserGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LearnHowToUserGit/LearnHowToUserGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LearnHowToUserGit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnHowToUserGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
